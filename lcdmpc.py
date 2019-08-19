90b6b4896faee1782f2dea080b8e04544abf1d74
Bay <cbay@nrel.gov>
2019-08-19 13:06:38 -0600

    coding up system matrices

--- lcdmpc.py
+++ lcdmpc.py
@@ -10,13 +10,20 @@
 # license for the specific language governing permissions and limitations under 
 # the License.
 
+import numpy as np
+from numpy import dot as dot
+from numpy import transpose as tp
+from numpy.linalg import matrix_power as matpower
+
 class LCDMPC():
     def __init__(self):
         self.subsystems = []
 
-    def build_subsystem(self, A, B, C, D, inputs, outputs, ID=None):
+    def build_subsystem(self, A, Bu, Bv, Bd, Cy, Cz, Dy, Dz, inputs, outputs, 
+                        horiz_len, nodeID=None, nodeName=None):
         # create subsystem object
-        subsys = subsystem(self, A, B, C, D, inputs, outputs, ID=ID)
+        subsys = subsystem(self, A, Bu, Bv, Bd, Cy, Cz, Dy, Dz, inputs, \
+            outputs, horiz_len, nodeID=nodeID, nodeName=nodeName)
         # append it to subsystem list
         self.subsystems.append(subsys)
         
@@ -43,13 +50,20 @@ class LCDMPC():
         pass
 
 class subsystem():
-    def __init__(self, obj, A, B, C, D, inputs, outputs, upstream=None, downstream=None, ID=None):
+    def __init__(self, obj, A, Bu, Bv, Bd, Cy, Cz, Dy, Dz, 
+                 inputs, outputs, horiz_len, upstream=None, 
+                 downstream=None, nodeID=None, nodeName=None):
         self.A = A
-        self.B = B
-        self.C = C
-        self.D = D
+        self.Bu = Bu
+        self.Bv = Bv
+        self.Bd = Bd
+        self.Cy = Cy
+        self.Cz = Cz
+        self.Dy = Dy
+        self.Dz = Dz
         self.inputs = inputs
         self.outputs = outputs
+        self.horiz_len = horiz_len
         if upstream is not None:
             self.upstream = upstream
         else:
@@ -58,12 +72,18 @@ class subsystem():
             self.downstream = downstream
         else:
             self.downstream = []
-        if ID is not None:
-            self.ID = ID
+        if nodeID is not None:
+            self.nodeID = nodeID
         else:
-            self.ID = str(len(obj.subsystems) + 1)
+            self.nodeID = str(len(obj.subsystems) + 1)
+        if nodeName is not None:
+            self.nodeName = nodeName
+        else:
+            self.nodeName = 'Node ' + str(len(obj.subsystems) + 1)
 
-    def set_opt_bounds(self):
+        self.mat_sizes()
+
+    def set_opt_bounds(self, low, high):
         pass
 
     def optimize(self):
@@ -71,15 +91,182 @@ class subsystem():
 
     def obj_func(self, control):
         # U^T*H*U + 2*U^T*F + V^T*E*V + 2*V^T*T
+        self.update()
         self.U = control
-        return np.transpose(self.U)*self.H*self.U + 2*np.transpose(self.U)*F + np.transpose(self.V)*self.E*self.V + 2*np.transpose(self.V)*self.T
+        return dot(dot(tp(self.U), self.H), self.U) + 2*dot(tp(self.U), F) \
+            + dot(dot(tp(self.V), self.E), self.V) + 2*dot(tp(self.V), self.T)
 
     def update(self):
-        self.H = np.transpose(self.My)*self.Q*self.My + self.S
+        self.H = dot(dot(tp(self.My), self.Q), self.My) + self.S
         self.E_1 = self.E
-        self.E = np.transpose(self.Ny)*self.Q*self.Ny
-        self.F = np.transpose(self.My)*self.Q*(self.Fy*self.x0 + self.Ny*self.V - self.ref) + 0.5*np.transpose(self.Mz)*self.psi
-        self.T = np.transpose(self.Ny)*self.Q*(self.Fy*self.x0 - self.ref) + 0.5*np.transpose(self.Nz)*self.psi
+        self.E = dot(dot(tp(self.Ny), self.Q), self.Ny)
+        self.F = dot(dot(tp(self.My), self.Q), (dot(self.Fy, self.x0) \
+               + dot(self.Ny, self.V) - self.ref)) + 0.5*dot(tp(self.Mz), self.psi)
+        self.T = dot(dot(tp(self.Ny), self.Q), (dot(self.Fy, self.x0) - self.ref)) \
+               + 0.5*dot(tp(self.Nz), self.psi)
 
     def calc_sens(self):
-        self.gamma = 2*(self.E_1*self.V + self.T + np.transpose(self.Ny)*self.Q*self.My*self.U)
+        self.gamma = 2*(dot(self.E_1, self.V) + self.T \
+                   + dot(dot(dot(tp(self.Ny), self.Q), self.My), self.U))
+
+    def sys_matrices(self):       
+        self.Fy = np.array([dot(self.Cy, matpower(self.A, i)) \
+                  for i in range(1, self.horiz_len + 1)])
+        if self.Fy[0].ndim > 1:
+            self.Fy = np.concatenate(self.Fy, axis=1)
+        else:
+            self.Fy = np.concatenate(self.Fy)
+
+        self.Fz = np.array([dot(self.Cz, matpower(self.A, i)) \
+                  for i in range(0, self.horiz_len)])
+        if self.Fz[0].ndim > 1:
+            self.Fz = np.concatenate(self.Fz, axis=1)
+        else:
+            self.Fz = np.concatenate(self.Fz)
+
+        Mytmp = dot(self.Cy, self.Bu)
+        MytmpShape = np.shape(Mytmp)
+        for i in range(self.horiz_len - 1):
+            Mytmp = np.hstack((Mytmp, np.zeros(MytmpShape)))
+        for i in range(1, self.horiz_len):
+            if Mytmp.ndim == 1:
+                Mytmp = np.vstack((Mytmp, np.hstack((dot(self.Cy, \
+                    dot(matpower(self.A, i), self.Bu )), Mytmp[:-self.nyBu]))))
+            else:
+                if dot(self.Cy, dot(matpower(self.A, i), self.Bu )).ndim == 1:
+                    Mytmp = np.vstack((Mytmp, np.hstack((dot(self.Cy, \
+                        dot(matpower(self.A, i), self.Bu )), Mytmp[-self.nxCy:,:-self.nyBu][0]))))
+                else:
+                    Mytmp = np.vstack((Mytmp, np.hstack((dot(self.Cy, \
+                        dot(matpower(self.A, i), self.Bu )), Mytmp[-self.nxCy:,:-self.nyBu]))))
+        self.My = Mytmp
+
+        Mztmp0 = self.Dz
+        Mztmp0Shape = np.shape(Mztmp0)
+        Mztmp = np.hstack((dot(self.Cz, self.Bu), Mztmp0))
+        for i in range(self.horiz_len - 1):
+            Mztmp0 = np.hstack((Mztmp0, np.zeros(Mztmp0Shape)))
+        for i in range(self.horiz_len - 2):
+            Mztmp = np.hstack((Mztmp, np.zeros(Mztmp0Shape)))
+        Mztmp = np.vstack((Mztmp0, Mztmp))
+        for i in range(1, self.horiz_len - 1):
+            if Mztmp.ndim == 1:
+                Mztmp = np.vstack((Mztmp, np.hstack((dot(self.Cz, \
+                    dot(matpower(self.A, i), self.Bu )), Mztmp[:-self.nyBu]))))
+            else:
+                if dot(self.Cz, dot(matpower(self.A, i), self.Bu )).ndim == 1:
+                    Mztmp = np.vstack((Mztmp, np.hstack((dot(self.Cz, \
+                        dot(matpower(self.A, i), self.Bu )), Mztmp[-self.nxCz:,:-self.nyBu][0]))))
+                else:
+                    Mztmp = np.vstack((Mztmp, np.hstack((dot(self.Cz, \
+                        dot(matpower(self.A, i), self.Bu )), Mztmp[-self.nxCz:,:-self.nyBu]))))
+        self.Mz = Mztmp
+        
+        Nytmp = dot(self.Cy, self.Bv)
+        NytmpShape = np.shape(Nytmp)
+        for i in range(self.horiz_len - 1):
+            Nytmp = np.hstack((Nytmp, np.zeros(NytmpShape)))
+        for i in range(1, self.horiz_len):
+            if Nytmp.ndim == 1:
+                Nytmp = np.vstack((Nytmp, np.hstack((dot(self.Cy, \
+                    dot(matpower(self.A, i), self.Bv )), Nytmp[:-self.nyBv]))))
+            else:
+                if dot(self.Cy, dot(matpower(self.A, i), self.Bv )).ndim == 1:
+                    Nytmp = np.vstack((Nytmp, np.hstack((dot(self.Cy, \
+                        dot(matpower(self.A, i), self.Bv )), Nytmp[-self.nxCy:,:-self.nyBv][0]))))
+                else:
+                    Nytmp = np.vstack((Nytmp, np.hstack((dot(self.Cy, \
+                        dot(matpower(self.A, i), self.Bv )), Nytmp[-self.nxCy:,:-self.nyBv]))))
+        self.Ny = Nytmp
+        
+        Nztmp = dot(self.Cz, self.Bv)
+        NztmpShape = np.shape(Nztmp)
+        for i in range(self.horiz_len - 1):
+            Nztmp = np.hstack((Nztmp, np.zeros(NztmpShape)))
+        Nztmp0 = np.zeros(np.shape(Nztmp))
+        Nztmp = np.vstack((Nztmp0, Nztmp))
+        for i in range(1, self.horiz_len - 1):
+            if Nztmp.ndim == 1:
+                Nztmp = np.vstack((Nztmp, np.hstack((dot(self.Cz, \
+                    dot(matpower(self.A, i), self.Bv )), Nztmp[:-self.nyBv]))))
+            else:
+                if dot(self.Cz, dot(matpower(self.A, i), self.Bv )).ndim == 1:
+                    Nztmp = np.vstack((Nztmp, np.hstack((dot(self.Cz, \
+                        dot(matpower(self.A, i), self.Bv )), Nztmp[-self.nxCz:,:-self.nyBv][0]))))
+                else:
+                    Nztmp = np.vstack((Nztmp, np.hstack((dot(self.Cz, \
+                        dot(matpower(self.A, i), self.Bv )), Nztmp[-self.nxCz:,:-self.nyBv]))))
+        self.Nz = Nztmp
+
+    def mat_sizes(self):
+        if self.A.ndim == 1:
+            self.nxA = 1
+            self.nyA = 0
+        elif self.A.ndim == 2:
+            self.nxA = np.shape(self.A)[0]
+            self.nyA = np.shape(self.A)[1]
+        else:
+            raise Exception("The 'A' matrix has > 2 dimensions")
+
+        if self.Bu.ndim == 1:
+            self.nxBu = 1
+            self.nyBu = 0
+        elif self.Bu.ndim == 2:
+            self.nxBu = np.shape(self.Bu)[0]
+            self.nyBu = np.shape(self.Bu)[1]
+        else:
+            raise Exception("The 'Bu' matrix has > 2 dimensions")
+        
+        if self.Bv.ndim == 1:
+            self.nxBv = 1
+            self.nyBv = 0
+        elif self.Bv.ndim == 2:
+            self.nxBv = np.shape(self.Bv)[0]
+            self.nyBv = np.shape(self.Bv)[1]
+        else:
+            raise Exception("The 'Bv' matrix has > 2 dimensions")
+
+        if self.Bd.ndim == 1:
+            self.nxBd = 1
+            self.nyBd = 0
+        elif self.Bd.ndim == 2:
+            self.nxBd = np.shape(self.Bd)[0]
+            self.nyBd = np.shape(self.Bd)[1]
+        else:
+            raise Exception("The 'Bd' matrix has > 2 dimensions")
+
+        if self.Cy.ndim == 1:
+            self.nxCy = 1
+            self.nyCy = 0
+        elif self.Cy.ndim == 2:
+            self.nxCy = np.shape(self.Cy)[0]
+            self.nyCy = np.shape(self.Cy)[1]
+        else:
+            raise Exception("The 'Cy' matrix has > 2 dimensions")
+
+        if self.Cz.ndim == 1:
+            self.nxCz = 1
+            self.nyCz = 0
+        elif self.Cz.ndim == 2:
+            self.nxCz = np.shape(self.Cz)[0]
+            self.nyCz = np.shape(self.Cz)[1]
+        else:
+            raise Exception("The 'Cz' matrix has > 2 dimensions")
+
+        if self.Dy.ndim == 1:
+            self.nxDy = 1
+            self.nyDy = 0
+        elif self.Dy.ndim == 2:
+            self.nxDy = np.shape(self.Dy)[0]
+            self.nyDy = np.shape(self.Dy)[1]
+        else:
+            raise Exception("The 'Dy' matrix has > 2 dimensions")
+
+        if self.Dz.ndim == 1:
+            self.nxDz = 1
+            self.nyDz = 0
+        elif self.Dz.ndim == 2:
+            self.nxDz = np.shape(self.Dz)[0]
+            self.nyDz = np.shape(self.Dz)[1]
+        else:
+            raise Exception("The 'Dz' matrix has > 2 dimensions")
--- main.py
+++ main.py
@@ -11,20 +11,56 @@
 # the License.
 
 import lcdmpc as opt
+import numpy as np
+# import numpy.linalg.matrix_power as matpow
 
 tmp = opt.LCDMPC()
 
-A = 1
-B = 2
-C = 3
-D = 4
+horiz_len = 5
+
+# A = np.mat([[1, 0], 
+#             [0, 2]])
+# Bu = np.mat([[2, 0], 
+#              [0, 1]])
+# Bv = np.mat([[0, 0], 
+#              [1, 0]])
+# Bd = np.zeros((2, 2))
+# Cy = np.mat([0, 5])
+# Cz = np.zeros((1, 2))
+# Dy = np.mat([0, 2])
+# Dz = np.zeros((1, 2))
+
+A = np.array([[2, 4], 
+            [6, 8]])
+Bu = np.array([[1, 1], 
+             [1, 1]])
+Bv = np.array([[0, 0], 
+             [1, 0]])
+Bd = np.array([[2], [2]])
+Cy = np.array([1, 1])
+# Cy = np.array([[1, 1], [0, 1]])
+# Cz = np.array([2, 2])
+Cz = np.array([[2, 2], [1, 1]])
+Dy = np.array([[0, 2], [0, 0]])
+Dz = np.array([[1, 2], [1, 0]])
 
 inputs = [1,2,3]
 outputs = [4,5]
 ID = 'building 1'
 
-tmp.build_subsystem(A, B, C, D, inputs, outputs, ID=ID)
-tmp.build_subsystem(A, B, C, D, inputs, outputs, ID='building 2')
+tmp.build_subsystem(A, Bu, Bv, Bd, Cy, Cz, Dy, Dz, inputs, outputs, horiz_len)
+tmp.build_subsystem(A, Bu, Bv, Bd, Cy, Cz, Dy, Dz, inputs, outputs, horiz_len)
 
-print(tmp.subsystems[0].ID)
-print(tmp.subsystems[1].ID)
+tmp.subsystems[0].sys_matrices()
+
+np.set_printoptions(suppress=True)
+
+# print(np.array(tmp.subsystems[0].Fy))
+# print(np.array(tmp.subsystems[0].Fz))
+print(tmp.subsystems[0].Nz)
+
+# print(tmp.subsystems[0].nodeID)
+# print(tmp.subsystems[1].nodeID)
+
+# print(tmp.subsystems[0].nodeName)
+# print(tmp.subsystems[1].nodeName)
