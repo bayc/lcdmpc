b6c202e9911e9457dfb2f9c638dbe5eb712c1ddd
Chintala <rchintal@nrel.gov>
2019-08-20 17:37:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,76 @@
 # license for the specific language governing permissions and limitations under 
 # the License.
 
+import lcdmpc as opt
+import numpy as np
+# import numpy.linalg.matrix_power as matpow
+
+tmp = opt.LCDMPC()
+
+horiz_len = 5
+
+# A = np.mat([[1, 0], 
+#             [0, 2]])
+# Bu = np.mat([[2, 0], 
+#              [0, 1]])
+# Bv = np.mat([[0, 0], 
+#              [1, 0]])
+# Bd = np.zeros((2, 2))
+# Cy = np.mat([0, 5])
+# Cz = np.zeros((1, 2))
+# Dy = np.mat([0, 2])
+# Dz = np.zeros((1, 2))
+
+A = np.array([[2, 4], 
+            [6, 8]])
+Bu = np.array([[1, 1], 
+             [1, 1]])
+Bv = np.array([[0, 0], 
+             [1, 0]])
+Bd = np.array([[2], [2]])
+Cy = np.array([1, 1])
+# Cy = np.array([[1, 1], [0, 1]])
+# Cz = np.array([2, 2])
+Cz = np.array([[2, 2], [1, 1]])
+Dy = np.array([[0, 2], [0, 0]])
+Dz = np.array([[1, 2], [1, 0]])
+
+inputs = [1,2,3]
+outputs1 = [4,5]
+outputs2 = [55, 77]
+ID = 'building 1'
+
+tmp.build_subsystem(A, Bu, Bv, Bd, Cy, Cz, Dy, Dz, inputs, outputs1, horiz_len)
+tmp.build_subsystem(A, Bu, Bv, Bd, Cy, Cz, Dy, Dz, inputs, outputs2, horiz_len)
+
+tmp.subsystems[0].sys_matrices()
+
+connections = [[0, 1], [1, 0]]
+
+tmp.build_interconnections(interconnections=connections)
+
+np.set_printoptions(suppress=True)
+
+tmp.update_outputs()
+tmp.communicate()
+
+print(tmp.subsystems[0].v)
+print(tmp.subsystems[1].v)
+
+# print(tmp.subsystems[0].nodeID)
+# print(tmp.subsystems[0].downstream)
+# print(tmp.subsystems[0].upstream)
+
+# print(tmp.subsystems[1].nodeID)
+# print(tmp.subsystems[1].downstream)
+# print(tmp.subsystems[1].upstream)
+
+# print(np.array(tmp.subsystems[0].Fy))
+# print(np.array(tmp.subsystems[0].Fz))
+# print(tmp.subsystems[0].Nz)
+
+# print(tmp.subsystems[0].nodeID)
+# print(tmp.subsystems[1].nodeID)
+
+# print(tmp.subsystems[0].nodeName)
+# print(tmp.subsystems[1].nodeName)
