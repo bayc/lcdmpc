ba90b4505ff468b0841ef19ea4860461069a8441
rchintala13 <rohit.chintala@nrel.gov>
2019-09-06 11:59:14 -0600

    Update bldg_data_driven_mdl.py

--- bldg_data_driven_mdl.py
+++ bldg_data_driven_mdl.py
@@ -7,10 +7,11 @@ Created on Wed Aug 14 13:35:17 2019
 import numpy as np
 
 class bldg_data_driven_mdl:
-    self.A = 0.9981
-    self.Bu = 0.0374; # coefficients correspondig to Q_hvac - self.mean_inputs(4)
-    self.Bd = [0.0038,0.0024,0.0762] #[T_out - T_room, Q_int, Q_solar] - self.mean_inputs(1:3)
-    self.C = 1
-    self.K = 0.9981
-    self.mean_inputs = np.array([-13.5548,0.736,0.2062,0.894]); 
-    self.mean_outputs = 21.8742
+    def __init__(self):
+        self.A = 0.8826
+        self.Bu = 0.0014 # coefficients correspondig to Q_hvac (-ve value for cooling) - self.mean_inputs(4)
+        self.Bd = [0.0116,0.0024,0.001] #[T_out , Q_int, Q_solar] - self.mean_inputs(1:3)
+        self.C = 1
+        self.K = 0.8826
+        self.mean_inputs = np.array([19.96,0,0,0]); 
+        self.mean_outputs = 23.56
